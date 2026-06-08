fc845c331e5bfa443165ee2c4c4a8b2d81203d95
inacinhosky <inaciomartinsalves12@outlook.com>
2026-06-08 01:08:17 -0300

    Falas e fadeIn e FadeOut

--- Assets/ScriptsJogo/Dialogo.cs
+++ Assets/ScriptsJogo/Dialogo.cs
@@ -8,18 +8,20 @@ public class Dialogo : MonoBehaviour
     public string[] lines;
     public float textSpeed;
     private int index;
-
     public GameObject fadeOutAnimacao;
+    public GameObject fadeInAnimacao;
     public GameObject quarto;
+    public GameObject cozinha;
+    public GameObject caixaDialogo;
     public GameObject background;
     private bool introducaoFeita = false;
     private bool transicaoAcontecendo;
-
     void Start()
     {
+        caixaDialogo.SetActive(false);
         textComponent.text = string.Empty;
-        StartDialogue();
         fadeOutAnimacao.SetActive(false);
+        fadeInAnimacao.SetActive(false);
         quarto.SetActive(false);
     }
 
@@ -31,12 +33,22 @@ void Update()
     }
     if (Input.GetMouseButtonDown(0))
     {
+        if (!caixaDialogo.activeSelf)
+        {
+            caixaDialogo.SetActive(true);
+            StartDialogue();
+            return;
+        }
         if (textComponent.text == lines[index])
         {
             if (!introducaoFeita && index == 0)
             {
                 introducaoFeita = true;
                 StartCoroutine(Introdução());
+            }
+            else if (index == 3)
+            {
+                StartCoroutine(TransicaoCena(quarto, cozinha));
             }
             else
             {
@@ -50,14 +62,12 @@ void Update()
         }
     }
 }
-
     void StartDialogue()
     {
         index = 0;
         StartCoroutine(TypeLine());
     }
-
-    IEnumerator TypeLine()
+    IEnumerator TypeLine() // transforma as frases em caracteres para que tenha o efeito de ir de letra por letra dentro do dialogo.
     {
         foreach (char c in lines[index].ToCharArray())
         {
@@ -65,7 +75,7 @@ void Update()
             yield return new WaitForSeconds(textSpeed);
         }
     }
-    IEnumerator Introdução()
+    IEnumerator Introdução() // Corrotina para o inicio de tela preta.
     {
         transicaoAcontecendo = true;
         quarto.SetActive(true);
@@ -74,9 +84,10 @@ void Update()
         yield return new WaitForSeconds(2.5f);
         NextLine();
         transicaoAcontecendo = false;
+        fadeOutAnimacao.SetActive(false);
     }
 
-    void NextLine()
+    void NextLine() // função feita para ir para a próxima linha
     {
         if (index < lines.Length - 1)
         {
@@ -88,5 +99,21 @@ void Update()
         {
             gameObject.SetActive(false);
         }
+    }
+    IEnumerator TransicaoCena(GameObject cenarioAtual, GameObject novoCenario) // corrotina usada sempre que for mudar de um cenário para o outro
+    {
+    transicaoAcontecendo = true;
+    fadeInAnimacao.SetActive(true);
+    yield return new WaitForSeconds(2.5f);
+    Debug.Log("Cenário ta mudando");
+    cenarioAtual.SetActive(false);
+    novoCenario.SetActive(true);
+    fadeOutAnimacao.SetActive(true);
+    fadeInAnimacao.SetActive(false);
+    yield return new WaitForSeconds(2.5f);
+    NextLine();
+    transicaoAcontecendo = false;
+    fadeInAnimacao.SetActive(false);
+    fadeOutAnimacao.SetActive(false);
     }
 }
